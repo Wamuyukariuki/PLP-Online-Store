2348bbd1d92429f443bc9d2a8c707293c1034fe3
Wamuyukariuki <jaxonkariuki@gmail.com>
2024-04-16 16:02:39 +0300

    models for store

--- category/models.py
+++ category/models.py
@@ -1,6 +1,28 @@
 from django.db import models
+from django.urls import reverse
 
 
 # Create your models here.
-class category(models.Model):
-    name = models.CharField(max_length=200)
+class Category(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    keyword = models.TextField()
+    image = models.ImageField(null=True, blank=True)
+    created = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now=True)
+    STATUS = (
+        ('True', 'True'),
+        ('False', 'False')
+    )
+    status = models.CharField
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def get_url(self):
+        return reverse('products_bycategory', args=[self.slug])
+
+    def __str__(self):
+        return self.name
--- store/models.py
+++ store/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.urls import reverse
+from category.models import Category
+
 
 # Create your models here.
+
+class Products(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    description = models.TextField(null=True, blank=True, max_length=10000)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.DecimalField(max_length=10, decimal_places=2)
+    image = models.ImageField(null=True, blank=True)
+    stock = models.IntegerField()
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
+
+    def get_url(self):
+        return reverse('product_details', args=[self.category.slag, self.slug])
+
+    @property
+    def ImageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+            return url
