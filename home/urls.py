f3c760d7df353ef5d848860660b4ca093449d254
Wamuyukariuki <jaxonkariuki@gmail.com>
2024-09-16 16:58:55 +0300

    updated the home page

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
-
 from . import views
-from .views import home
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home_view, name='home'),
 ]
